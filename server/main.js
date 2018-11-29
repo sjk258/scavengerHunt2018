38150efa4db3d5ad91d31406a7d9b8e307bf6f6f
Steve Kusner <sjk258@gmail.com>
2018-11-28 22:14:29 -0500

    adding status, removing stringify

--- server/main.js
+++ server/main.js
@@ -8,10 +8,11 @@ Router.route('/intro', {where: 'server'})
   .post(function(){
     var response = {
       "error" : false,
+      "status" : "200",
       "message" : {
         "text": "Ho Ho Ho!"
       }
     };
     this.response.setHeader('Content-Type', 'application/json');
-    this.response.end(JSON.stringify(response));
+    
   });
